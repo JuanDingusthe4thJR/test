c3682c5f68231aa2386b834426ff05ac81a15159
JuanDingusthe4thJR <epicnotfunnyatall@gmail.com>
2026-05-06 20:19:46 -0700

    Add files via upload

---
+++ gemini-code-1778123921202.js
@@ -0,0 +1,18 @@
+const apps = [
+    {
+        name: "Google",
+        url: "https://www.google.com/search?igu=1" // Note: Some sites block iFrames
+    },
+    {
+        name: "Discord",
+        url: "https://discord.com/app"
+    },
+    {
+        name: "Excalidraw",
+        url: "https://excalidraw.com/"
+    },
+    {
+        name: "Calculator",
+        url: "https://www.desmos.com/scientific"
+    }
+];
